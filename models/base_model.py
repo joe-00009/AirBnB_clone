5bed8a496b5350cfa2757bf9f04bf384c7b91356
Joe-0009 <youssefrachidi1994@gmail.com>
2024-01-14 03:46:27 -0800

    first tasl

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines the BaseModel Class."""
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+    
+    def __init__(self, *args, **kwargs):
+        """
+
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        rerutn f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+
+        return obj_dict
+
+
---
+++ models/engine/file_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
+import json
+
+
+
+class FileStorage:
+    """ """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
