3a56205bb6822f19e8cd309f78b5e6910a701fc5
Joe-0009 <youssefrachidi1994@gmail.com>
2024-01-14 07:51:20 -0800

    gg

--- console.py
+++ console.py
@@ -49,60 +49,75 @@ class HBNBCommand(cmd.Cmd):
         else:
             print(objects[key])
 
-    def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id."""
-        args = arg.split()
+    def do_destroy(self, args):
+        """ Deletes an instance based on the class name and id """
         if not args:
             print("** class name missing **")
-        elif args[0] not in storage.classes:
+            return
+        args_list = args.split()
+        try:
+            cls = eval(args_list[0])
+        except NameError:
             print("** class doesn't exist **")
-        elif len(args) < 2:
+            return
+        if len(args_list) < 2:
             print("** instance id missing **")
+            return
+        key = "{}.{}".format(args_list[0], args_list[1])
+        objects = storage.all()
+        if key not in objects:
+            print("** no instance found **")
         else:
-            key = "{}.{}".format(args[0], args[1])
-            if key in storage.all():
-                del storage.all()[key]
-                storage.save()
-            else:
-                print("** no instance found **")
+            del objects[key]
+            storage.save()
 
-    def do_all(self, arg):
-        """Prints all string representation of all instances."""
-        args = arg.split()
+    def do_all(self, args):
+        """ Prints all string representations of all instances """
+        objects = storage.all()
         obj_list = []
         if not args:
-            for key, value in storage.all().items():
-                obj_list.append(str(value))
+            for obj in objects.values():
+                obj_list.append(str(obj))
             print(obj_list)
-        elif args[0] not in storage.classes:
-            print("** class doesn't exist **")
         else:
-            for key, value in storage.all().items():
-                if key.split(".")[0] == args[0]:
-                    obj_list.append(str(value))
+            try:
+                cls = eval(args)
+            except NameError:
+                print("** class doesn't exist **")
+                return
+            for key, obj in objects.items():
+                if key.split(".")[0] == args:
+                    obj_list.append(str(obj))
             print(obj_list)
 
-    def do_update(self, arg):
-        """Updates an instance based on the class name and id."""
-        args = arg.split()
+    def do_update(self, args):
+        """ Updates an instance based on the class name and id """
         if not args:
             print("** class name missing **")
-        elif args[0] not in storage.classes:
+            return
+        args_list = args.split()
+        try:
+            cls = eval(args_list[0])
+        except NameError:
             print("** class doesn't exist **")
-        elif len(args) < 2:
+            return
+        if len(args_list) < 2:
             print("** instance id missing **")
-        elif len(args) < 3:
+            return
+        key = "{}.{}".format(args_list[0], args_list[1])
+        objects = storage.all()
+        if key not in objects:
+            print("** no instance found **")
+            return
+        if len(args_list) < 3:
             print("** attribute name missing **")
-        elif len(args) < 4:
+            return
+        if len(args_list) < 4:
             print("** value missing **")
-        else:
-            key = "{}.{}".format(args[0], args[1])
-            if key in storage.all():
-                instance = storage.all()[key]
-                setattr(instance, args[2], eval(args[3]))
-                instance.save()
-            else:
-                print("** no instance found **")
+            return
+        obj = objects[key]
+        setattr(obj, args_list[2], args_list[3])
+        obj.save()
 
 
 if __name__ == '__main__':
