d73e6eee2df63628084e6e0707c1bcdf37f822fb
Joe-0009 <youssefrachidi1994@gmail.com>
2024-01-14 13:42:38 -0800

    Gg

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
             **kwargs (dict): Key/value pairs of attributes.
         """
         t_form = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
