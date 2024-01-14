93e1ce77c980a63ea3d6c755e8a96ee7d866b8fc
Joe-0009 <youssefrachidi1994@gmail.com>
2024-01-14 06:41:39 -0800

    gg

--- models/base_model.py
+++ models/base_model.py
@@ -3,15 +3,17 @@
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
     """Defines the BaseModel class."""
 
     def __init__(self, *args, **kwargs):
         """Initialize a new instance of BaseModel."""
+        gg = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key in ["created_at", "updated_at"]:
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key, datetime.strptime(value, gg))
                 elif key != "__class__":
                     setattr(self, key, value)
         else:
@@ -21,14 +23,14 @@ class BaseModel:
 
     def __str__(self):
         """Return a string representation of the instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update the public instance attribute updated_at with the current datetime."""
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return a dictionary containing all keys/values of __dict__ of the instance."""
+        """Return a dictionary containing all keys/values of __dict__"""
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
