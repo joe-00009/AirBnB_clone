3adcd9d080dae8df581ac4042f17402c1851197c
Joe-0009 <youssefrachidi1994@gmail.com>
2024-01-14 04:06:24 -0800

    first tasl

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,6 @@
 import json
 
 
-
 class FileStorage:
     """ """
     __file_path = "file.json"
@@ -16,3 +15,20 @@ class FileStorage:
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
+    def save(self):
+        serialized_objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as file:
+                loaded_objects = json.load(file)
+                for key, value in loaded_objects.items():
+                    class_name, obj_id = key.split('.')
+                    if class_name == 'BaseModel':
+                        from models.base_model import BaseModel
+                        value['created_at'] = datetime.strptime(value['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                        value['updated_at'] = datetime.strptime(value['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                        obj = BaseModel(**value)
+                        self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
